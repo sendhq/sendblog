9a7ffcd43e54c8332736ba30dd5e7e7b71e8face
Richard Igbiriki <imorobebh@gmail.com>
2020-10-15 11:02:07 +0100

    update blog page

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
 import { Helmet } from 'react-helmet'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+
 import facebook from '../img/social/facebook.svg'
 import twitter from '../img/social/twitter.svg'
 import linkedin from '../img/social/linkedin.svg'
-
+import Img from 'gatsby-image'
 export const BlogPostTemplate = ({
   content,
   contentComponent,
@@ -16,7 +16,9 @@ export const BlogPostTemplate = ({
   tags,
   title,
   helmet,
+  featuredImage,
   slug,
+  date,
 }) => {
   const PostContent = contentComponent || Content
 
@@ -27,60 +29,76 @@ export const BlogPostTemplate = ({
   const handleLinkClick = (link) => {
     window.open(link, '_blank')
   }
+
   return (
-    <section className="section">
+    <div className="body_wrap">
       {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map((tag) => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-            <h3>Share via</h3>
-            <div className=" social-section">
-              <div
-                tabIndex={0}
-                role="button"
-                onKeyPress={() => handleLinkClick(fb)}
-                onClick={() => handleLinkClick(fb)}
-              >
-                <img src={facebook} alt="Facebook" className="icon" />
-              </div>
-              <div
-                tabIndex={0}
-                role="button"
-                onKeyPress={() => handleLinkClick(twitterLink)}
-                onClick={() => handleLinkClick(twitterLink)}
-              >
-                <img className="fas fa-lg icon" src={twitter} alt="Twitter" />
-              </div>
-              <div
-                tabIndex={0}
-                role="button"
-                onKeyPress={() => handleLinkClick(linkedinLink)}
-                onClick={() => handleLinkClick(linkedinLink)}
-              >
-                <img src={linkedin} className="icon" alt="Linkedin" />
-              </div>
-            </div>
+      <div className="top back_sky clearfix">
+        <div className="contain post_header pt4">
+          <p className="m-b category_date pt">
+            <a href="" className="_bold uppercased">
+              Blog &nbsp;/&nbsp; Product Updates
+            </a>
+          </p>
+
+          <h1>
+            <a href="">{title}</a>
+          </h1>
+          <p className="tagline mb4">{description}</p>
+          <div className="post_image">
+            <Img
+              fluid={featuredImage.childImageSharp.fluid}
+              style={{ height: '100%', width: '100%' }}
+            />
           </div>
         </div>
       </div>
-    </section>
+
+      <div className="clearfix post_body contain">
+        <div className="column col_3 share_icons ">
+          <div className="author mb4">
+            <span>
+              <img src="/img/missing.png" alt="Elijah" />
+            </span>
+            <span className="block_disp">Elijah Shokenu</span>
+          </div>
+
+          <p className="_bold small">Published On:</p>
+          <p>{date}</p>
+
+          <div className="pt4">
+            <div
+              tabIndex={0}
+              role="button"
+              onKeyPress={() => handleLinkClick(fb)}
+              onClick={() => handleLinkClick(fb)}
+            >
+              <img src={facebook} alt="Facebook" className="icon" />
+            </div>
+            <div
+              tabIndex={0}
+              role="button"
+              onKeyPress={() => handleLinkClick(twitterLink)}
+              onClick={() => handleLinkClick(twitterLink)}
+            >
+              <img className="fas fa-lg icon" src={twitter} alt="Twitter" />
+            </div>
+            <div
+              tabIndex={0}
+              role="button"
+              onKeyPress={() => handleLinkClick(linkedinLink)}
+              onClick={() => handleLinkClick(linkedinLink)}
+            >
+              <img src={linkedin} className="icon" alt="Linkedin" />
+            </div>
+          </div>
+        </div>
+
+        <div className="post_text column col_9">
+          <PostContent content={content} />
+        </div>
+      </div>
+    </div>
   )
 }
 
@@ -91,6 +109,8 @@ BlogPostTemplate.propTypes = {
   title: PropTypes.string,
   helmet: PropTypes.object,
   slug: PropTypes.string,
+  featuredImage: PropTypes.object,
+  date: PropTypes.string,
 }
 
 const BlogPost = ({ data }) => {
@@ -113,6 +133,8 @@ const BlogPost = ({ data }) => {
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
         slug={post.fields.slug}
+        featuredImage={post.frontmatter.featuredimage}
+        date={post.frontmatter.date}
       />
     </Layout>
   )
@@ -135,10 +157,17 @@ export const pageQuery = graphql`
         slug
       }
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "DD MMMM, YYYY")
         title
         description
         tags
+        featuredimage {
+          childImageSharp {
+            fluid(quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
