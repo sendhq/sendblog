8f705a0412a812a712cea5d5ff668ae5d684735c
Excellence Oyeniran <oyeniranexcellenced@gmail.com>
2023-05-10 00:28:14 +0100

    setting up

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,26 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
-
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql, StaticQuery } from "gatsby";
+import PreviewCompatibleImage from "./PreviewCompatibleImage";
 
 const BlogRollTemplate = (props) => {
-  
   const { edges: posts } = props.data.allMarkdownRemark;
 
   return (
-    <div className="columns is-multiline">
+    <div className='columns is-multiline'>
       {posts &&
         posts.map(({ node: post }) => (
-          <div className="is-parent column is-6" key={post.id}>
+          <div className='is-parent column is-6' key={post.id}>
             <article
               className={`blog-list-item tile is-child box notification ${
-                post.frontmatter.featuredpost ? 'is-featured' : ''
+                post.frontmatter.featuredpost ? "is-featured" : ""
               }`}
             >
               <header>
                 {post?.frontmatter?.featuredimage && (
-                  <div className="featured-thumbnail">
+                  <div className='featured-thumbnail'>
                     <PreviewCompatibleImage
                       imageInfo={{
                         image: post.frontmatter.featuredimage,
@@ -34,16 +32,16 @@ const BlogRollTemplate = (props) => {
                       }}
                     />
                   </div>
-                ) }
-                <p className="post-meta">
+                )}
+                <p className='post-meta'>
                   <Link
-                    className="title has-text-primary is-size-4"
+                    className='title has-text-primary is-size-4'
                     to={post.fields.slug}
                   >
                     {post.frontmatter.title}
                   </Link>
                   <span> &bull; </span>
-                  <span className="subtitle is-size-5 is-block">
+                  <span className='subtitle is-size-5 is-block'>
                     {post.frontmatter.date}
                   </span>
                 </p>
@@ -52,7 +50,7 @@ const BlogRollTemplate = (props) => {
                 {post.excerpt}
                 <br />
                 <br />
-                <Link className="button" to={post.fields.slug}>
+                <Link className='button' to={post.fields.slug}>
                   Keep Reading →
                 </Link>
               </p>
@@ -60,8 +58,8 @@ const BlogRollTemplate = (props) => {
           </div>
         ))}
     </div>
-  )
-}
+  );
+};
 
 BlogRoll.propTypes = {
   data: PropTypes.shape({
@@ -69,8 +67,7 @@ BlogRoll.propTypes = {
       edges: PropTypes.array,
     }),
   }),
-}
-
+};
 
 export default function BlogRoll() {
   return (
@@ -100,7 +97,6 @@ export default function BlogRoll() {
                         quality: 100
                         layout: CONSTRAINED
                       )
-
                     }
                   }
                 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,41 +11,48 @@ const TemplateWrapper = ({ children }) => {
   return (
     <div>
       <Helmet>
-        <html lang="en" />
+        <html lang='en' />
         <title>{title}</title>
-        <meta name="description" content={description} />
+        <meta name='description' content={description} />
 
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
+          rel='apple-touch-icon'
+          sizes='180x180'
           href={`${withPrefix("/")}img/apple-touch-icon.png`}
         />
         <link
-          rel="icon"
-          type="image/png"
+          rel='icon'
+          type='image/png'
           href={`${withPrefix("/")}img/favicon-32x32.png`}
-          sizes="32x32"
+          sizes='32x32'
         />
         <link
-          rel="icon"
-          type="image/png"
+          rel='icon'
+          type='image/png'
           href={`${withPrefix("/")}img/favicon-16x16.png`}
-          sizes="16x16"
+          sizes='16x16'
         />
 
         <link
-          rel="mask-icon"
+          rel='mask-icon'
           href={`${withPrefix("/")}img/safari-pinned-tab.svg`}
-          color="#ff4400"
+          color='#ff4400'
         />
-        <meta name="theme-color" content="#fff" />
+        <meta name='theme-color' content='#fff' />
 
-        <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content="/" />
+        <meta property='og:type' content='business.business' />
+        <meta property='og:title' content={title} />
+        <meta property='og:url' content='/' />
         <meta
-          property="og:image"
+          property='og:image'
           content={`${withPrefix("/")}img/og-image.jpg`}
+        />
+        <meta name='twitter:card' content='summary' />
+        <meta name='twitter:site' content='@sendfreight' />
+        <meta property='og:site_name' content='The SEND Blog.' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap'
+          rel='stylesheet'
         />
       </Helmet>
       <Navbar />
