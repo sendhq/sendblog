466dab418072deec7180fac23bc3a9d049044f8b
Excellence Oyeniran <oyeniranexcellenced@gmail.com>
2023-05-10 12:34:50 +0100

    fix: more UI fixes

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,6 +2,8 @@ import * as React from "react";
 
 import Layout from "../../components/Layout";
 import BlogRoll from "../../components/BlogRoll";
+import TopPost from "../../components/TopPost";
+import CategoriesPage from "../categories";
 
 export default class BlogIndexPage extends React.Component {
   state = {
@@ -11,33 +13,20 @@ export default class BlogIndexPage extends React.Component {
     this.setState({ filterCategory });
   };
   render() {
+    const { filterCategory } = this.state;
     return (
       <Layout>
-        <div
-          className='full-width-image-container margin-top-0'
-          style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
-          }}
-        >
-          <h1
-            className='has-text-weight-bold is-size-1'
-            style={{
-              boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
-              backgroundColor: "#f40",
-              color: "white",
-              padding: "1rem",
-            }}
-          >
-            Latest Stories
-          </h1>
-        </div>
-        <section className='section'>
-          <div className='container'>
-            <div className='content'>
-              <BlogRoll />
-            </div>
+        <div className='top back_sky clearfix'>
+          <div className='contain top_content'>
+            <TopPost />
+            <CategoriesPage
+              filterCategory={filterCategory}
+              updateFilterCategory={this.updateFilterCategory}
+            />
           </div>
-        </section>
+        </div>
+
+        <BlogRoll filterCategory={filterCategory} />
       </Layout>
     );
   }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,10 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { kebabCase } from "lodash";
 import { Helmet } from "react-helmet";
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
+
+import facebook from "../img/social/facebook.svg";
+import twitter from "../img/social/twitter.svg";
+import linkedin from "../img/social/linkedin.svg";
+
+import { GatsbyImage as Img } from "gatsby-plugin-image";
 
 // eslint-disable-next-line
 export const BlogPostTemplate = ({
@@ -14,36 +19,90 @@ export const BlogPostTemplate = ({
   tags,
   title,
   helmet,
+  featuredImage,
+  slug,
+  date,
+  author,
+  category,
 }) => {
   const PostContent = contentComponent || Content;
+  const baseURL = `https://blog.send.ng${slug}`;
+  const fb = `https://www.facebook.com/sharer/sharer.php?u=${baseURL}`;
+  const twitterLink = `https://twitter.com/share?url=${baseURL}&text=${title}&via=sendDelivery`;
+  const linkedinLink = `https://www.linkedin.com/shareArticle?url=${baseURL}`;
+  const handleLinkClick = (link) => {
+    window.open(link, "_blank");
+  };
 
   return (
-    <section className="section">
+    <div className='body_wrap'>
       {helmet || ""}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map((tag) => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
+      <div className='top back_sky clearfix'>
+        <div className='contain post_header pt4'>
+          <p className='m-b category_date pt'>
+            <a href='##' className='_bold uppercased'>
+              {category}
+            </a>
+          </p>
+
+          <h1>
+            <a href='##'>{title}</a>
+          </h1>
+          <p className='tagline mb4'>{description}</p>
+          <div className='post_image'>
+            <Img
+              fluid={featuredImage?.childImageSharp.fluid}
+              style={{ height: "100%", width: "100%" }}
+            />
           </div>
         </div>
       </div>
-    </section>
+
+      <div className='clearfix post_body contain'>
+        <div className='column col_3  '>
+          <div className='author mb4'>
+            <span>
+              <img src='/img/missing.png' alt='Elijah' />
+            </span>
+            <span className='block_disp'>{author}</span>
+          </div>
+
+          <p className='_bold small'>Published On:</p>
+          <p>{date}</p>
+
+          <div className='pt4 share_icons'>
+            <div
+              tabIndex={0}
+              role='button'
+              onKeyPress={() => handleLinkClick(fb)}
+              onClick={() => handleLinkClick(fb)}
+            >
+              <img src={facebook} alt='Facebook' className='icon' />
+            </div>
+            <div
+              tabIndex={0}
+              role='button'
+              onKeyPress={() => handleLinkClick(twitterLink)}
+              onClick={() => handleLinkClick(twitterLink)}
+            >
+              <img className='fas fa-lg icon' src={twitter} alt='Twitter' />
+            </div>
+            <div
+              tabIndex={0}
+              role='button'
+              onKeyPress={() => handleLinkClick(linkedinLink)}
+              onClick={() => handleLinkClick(linkedinLink)}
+            >
+              <img src={linkedin} className='icon' alt='Linkedin' />
+            </div>
+          </div>
+        </div>
+
+        <div className='post_text column col_9 mb4'>
+          <PostContent content={content} />
+        </div>
+      </div>
+    </div>
   );
 };
 
@@ -65,10 +124,10 @@ const BlogPost = ({ data }) => {
         contentComponent={HTMLContent}
         description={post.frontmatter.description}
         helmet={
-          <Helmet titleTemplate="%s | Blog">
+          <Helmet titleTemplate='%s | Blog'>
             <title>{`${post.frontmatter.title}`}</title>
             <meta
-              name="description"
+              name='description'
               content={`${post.frontmatter.description}`}
             />
           </Helmet>
@@ -93,11 +152,23 @@ export const pageQuery = graphql`
     markdownRemark(id: { eq: $id }) {
       id
       html
+      fields {
+        slug
+      }
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "DD MMMM, YYYY")
         title
         description
         tags
+        author
+        category
+        featuredimage {
+          childImageSharp {
+            fluid(quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
