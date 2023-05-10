ec9012c882809e9792458bb7ea7ae11266dd562f
Excellence Oyeniran <oyeniranexcellenced@gmail.com>
2023-05-10 11:26:54 +0100

    fix: remove contact pages

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -8,13 +8,13 @@ export default class BlogIndexPage extends React.Component {
     return (
       <Layout>
         <div
-          className="full-width-image-container margin-top-0"
+          className='full-width-image-container margin-top-0'
           style={{
             backgroundImage: `url('/img/blog-index.jpg')`,
           }}
         >
           <h1
-            className="has-text-weight-bold is-size-1"
+            className='has-text-weight-bold is-size-1'
             style={{
               boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
               backgroundColor: "#f40",
@@ -25,9 +25,9 @@ export default class BlogIndexPage extends React.Component {
             Latest Stories
           </h1>
         </div>
-        <section className="section">
-          <div className="container">
-            <div className="content">
+        <section className='section'>
+          <div className='container'>
+            <div className='content'>
               <BlogRoll />
             </div>
           </div>
