3eb87a5ab2864e55151475f55fb218e575b580b0
Excellence Oyeniran <oyeniranexcellenced@gmail.com>
2023-05-10 10:01:08 +0100

    chore: made changes to the footer

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -2,64 +2,77 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link, graphql, StaticQuery } from "gatsby";
 import PreviewCompatibleImage from "./PreviewCompatibleImage";
+import MdList from "react-ionicons/lib/MdList";
 
-const BlogRollTemplate = (props) => {
-  const { edges: posts } = props.data.allMarkdownRemark;
+class BlogRoll extends React.Component {
+  filterPosts = (filterCategory, posts) => {
+    if (filterCategory === "All") return posts;
 
-  return (
-    <div className='columns is-multiline'>
-      {posts &&
-        posts.map(({ node: post }) => (
-          <div className='is-parent column is-6' key={post.id}>
-            <article
-              className={`blog-list-item tile is-child box notification ${
-                post.frontmatter.featuredpost ? "is-featured" : ""
-              }`}
-            >
-              <header>
-                {post?.frontmatter?.featuredimage && (
-                  <div className='featured-thumbnail'>
+    const filteredPosts = posts.filter(
+      ({ node: post }) => post.frontmatter.category === filterCategory
+    );
+
+    return filteredPosts;
+  };
+
+  render() {
+    const { data, filterCategory } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
+    const filteredPosts = this.filterPosts(filterCategory, posts);
+    return (
+      <div className='contain pt4'>
+        <div className='blog_posts'>
+          {filteredPosts &&
+            filteredPosts.map(({ node: post }) => (
+              <div key={post.frontmatter.title} className='one_post clearfix'>
+                <div className='post_image column right col_4'>
+                  <div>
                     <PreviewCompatibleImage
                       imageInfo={{
                         image: post.frontmatter.featuredimage,
                         alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                        width:
-                          post.frontmatter.featuredimage.childImageSharp
-                            .gatsbyImageData.width,
-                        height:
-                          post.frontmatter.featuredimage.childImageSharp
-                            .gatsbyImageData.height,
                       }}
                     />
                   </div>
-                )}
-                <p className='post-meta'>
-                  <Link
-                    className='title has-text-primary is-size-4'
-                    to={post.fields.slug}
-                  >
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <span className='subtitle is-size-5 is-block'>
-                    {post.frontmatter.date}
-                  </span>
-                </p>
-              </header>
-              <p>
-                {post.excerpt}
-                <br />
-                <br />
-                <Link className='button' to={post.fields.slug}>
-                  Keep Reading →
-                </Link>
-              </p>
-            </article>
-          </div>
-        ))}
-    </div>
-  );
-};
+                </div>
+
+                <div className='post_attr column col_8'>
+                  <p className='mb category_date'>
+                    <Link to='/'>{post.frontmatter.category}</Link>
+                    &nbsp;&nbsp;&#8226;&nbsp;&nbsp;
+                    <span>{post.frontmatter.date}</span>
+                  </p>
+
+                  <h2 className='col_11'>
+                    <Link className='max_line_text' to={post.fields.slug}>
+                      {post.frontmatter.title}
+                    </Link>
+                  </h2>
+                  <p className='tagline mb col_11 max_line_text'>
+                    {" "}
+                    {post.frontmatter.description}
+                  </p>
+
+                  <div className='author'>
+                    <span>
+                      <img src='img/missing.png' alt='Elijah' />
+                    </span>
+                    <span>{post.frontmatter.author}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          {filteredPosts.length === 0 && (
+            <div className='margin_center empty_container'>
+              <MdList fontSize='60px' />
+              <h1 className='empty_list'>No Posts</h1>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
 BlogRoll.propTypes = {
   data: PropTypes.shape({
@@ -69,7 +82,8 @@ BlogRoll.propTypes = {
   }),
 };
 
-export default function BlogRoll() {
+// eslint-disable-next-line
+export default (props) => {
   return (
     <StaticQuery
       query={graphql`
@@ -105,7 +119,9 @@ export default function BlogRoll() {
           }
         }
       `}
-      render={(data, count) => <BlogRollTemplate data={data} count={count} />}
+      render={(data, count) => (
+        <BlogRoll data={data} count={count} {...props} />
+      )}
     />
   );
-}
+};
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,114 +1,188 @@
-import * as React from "react";
-import { Link } from "gatsby";
+import React from "react";
+import "./css/fonts.css";
+import "./custom.css";
+import "./css/reset.css";
+import "./css/style.css";
+import IosPlane from "react-ionicons/lib/IosPlaneOutline";
+import IosBoatOutline from "react-ionicons/lib/IosBoatOutline";
+import LogoFacebook from "react-ionicons/lib/LogoFacebook";
+import LogoTwitter from "react-ionicons/lib/LogoTwitter";
+import LogoLinkedIn from "react-ionicons/lib/LogoLinkedin";
+import LogoInstagram from "react-ionicons/lib/LogoInstagram";
+import IosSettings from "react-ionicons/lib/IosSettings";
 
-import logo from "../img/logo.svg";
-import facebook from "../img/social/facebook.svg";
-import instagram from "../img/social/instagram.svg";
-import twitter from "../img/social/twitter.svg";
-import vimeo from "../img/social/vimeo.svg";
+const Footer = class extends React.Component {
+  state = {
+    email: "",
+    loading: false,
+  };
 
-const Footer = () => {
-  
+  subscribeToNewsLetter = () => {
+    const { email } = this.state;
+    const data = {
+      list_ids: ["65aa9a8a-b21b-4037-abc6-428bb69bf647"],
+      contacts: [
+        {
+          email,
+        },
+      ],
+    };
+    const auth = `Bearer ${process.env.GATSBY_SENDGRID_API_KEY}`;
+
+    fetch("https://api.sendgrid.com/v3/marketing/contacts", {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        "content-type": "application/json",
+        Authorization: auth,
+      },
+    })
+      .then((res) => res.json())
+      .then(() => {
+        this.setState({ submitted: true, email: "", loading: false });
+      })
+      .catch((error) => {
+        this.setState({ error: error, loading: false });
+      });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    this.subscribeToNewsLetter();
+  };
+  render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: "14em", height: "10em" }}
-          />
+      <div>
+        <div className='newsletter_box contain'>
+          <h3 className='color_hite'>Sign Up for Newsletters</h3>
+          <p className='mb3'>This is where you get the scoop in your inbox!</p>
+
+          <form
+            onSubmit={this.handleSubmit}
+            className='clearfix col_7 margin_center m-b'
+          >
+            <div className='clearfix'>
+              <div className='col_8 column'>
+                <input
+                  value={this.state.email}
+                  name='email'
+                  onChange={this.handleChange}
+                  type='text'
+                  placeholder='Email Address'
+                />
+              </div>
+              <div className='column col_4'>
+                <button
+                  disabled={this.state.loading}
+                  type='submit'
+                  className='btn btn_gree'
+                >
+                  {this.state.loading ? (
+                    <IosSettings color='white' rotate={true} fontSize='15px' />
+                  ) : (
+                    <p>Submit</p>
+                  )}
+                </button>
+              </div>
+            </div>
+            {this.state.submitted && (
+              <p className='newsletter_success'>
+                Thank you for subscribing to our newsletter
+              </p>
+            )}
+          </form>
+
+          <IosBoatOutline color='white' className='icon' fontSize='60px' />
+          <IosPlane color='white' className='plane icon' fontSize='60px' />
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div style={{ maxWidth: "100vw" }} className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: "1em", height: "1em" }}
-                  />
+
+        <footer className='color_white'>
+          <div className='margin_center container clearfix main_footer'>
+            <div className='col_6 column'>
+              <h4 className='color_white mb'>About Us</h4>
+              <p className='col_10'>
+                SEND is a digital freight forwarder and customs broker for
+                Africa. We manage the entire process of shipping freight by air
+                and ocean, including customs clearance and final delivery
+                &mdash; using software to provide instant quotes, document
+                management, cost transparency and control across your supply
+                chain.
+              </p>
+            </div>
+
+            <div className='col_6 column align_right'>
+              <div className='btns_social mb'>
+                <a
+                  rel='noopener noreferrer'
+                  target='_blank'
+                  title='facebook'
+                  href='https://www.facebook.com/sendhq'
+                >
+                  <LogoFacebook className='social_icons' color='white' />
                 </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: "1em", height: "1em" }}
-                  />
+                <a
+                  rel='noopener noreferrer'
+                  target='_blank'
+                  title='facebook'
+                  href='https://twitter.com/sendDelivery'
+                >
+                  <LogoTwitter className='social_icons' color='white' />
                 </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: "1em", height: "1em" }}
-                  />
+                <a
+                  rel='noopener noreferrer'
+                  target='_blank'
+                  title='facebook'
+                  href='https://instagram.com/send.ng'
+                >
+                  <LogoInstagram className='social_icons' color='white' />
                 </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: "1em", height: "1em" }}
-                  />
+                <a
+                  rel='noopener noreferrer'
+                  target='_blank'
+                  href='https://www.linkedin.com/company/send-freight'
+                >
+                  <LogoLinkedIn className='social_icons' color='white' />
                 </a>
               </div>
             </div>
           </div>
-        </div>
-      </footer>
+
+          <div className='bottom_footer'>
+            <div className='margin_center container small'>
+              <span className='mbr'>
+                &copy; Copyright 2020 SEND. All rights reserved
+              </span>
+              &#8226;
+              <a
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://www.send.ng/privacy.html'
+              >
+                Privacy
+              </a>
+              &#8226;
+              <a
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://www.send.ng/cookies.html'
+              >
+                Cookies
+              </a>
+            </div>
+          </div>
+        </footer>
+
+        <script src='https://unpkg.com/ionicons@5.0.0/dist/ionicons.js'></script>
+      </div>
     );
+  }
 };
 
 export default Footer;
